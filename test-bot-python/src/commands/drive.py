888d38909d387c6ad9e8118083d51b4ebc9697cd
Milo Weinberg <iapetus011@gmail.com>
2021-01-18 16:07:16 -0500

    add Drive command

--- test-bot-python/src/commands/drive.py
+++ test-bot-python/src/commands/drive.py
@@ -5,5 +5,10 @@ class Drive(Command):
     def __init__(self):
         super().__init__(self.__class__.__name__)
 
+        self.requires(self.getRobot().drive_train)
+
     def execute(self):
-        raise NotImplementedError
+        self.getRobot().drive_train.drive(
+            self.getRobot().xbox_controller.getY(0),  # left stick, y axis
+            self.getRobot().xbox_controller.getX(1)  # right stick, x axis
+        )
