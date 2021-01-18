e83873181bdfadb8309c4ce79a96d50edb3abbaf
Milo Weinberg <iapetus011@gmail.com>
2021-01-18 16:38:16 -0500

    import + use Aligner

--- test-bot-python/src/robot.py
+++ test-bot-python/src/robot.py
@@ -2,13 +2,16 @@ from commandbased import CommandBasedRobot
 import wpilib
 
 from subsystems.drive_train import DriveTrain
+from subsystems.aligner import Aligner
 import constants
 
 
 class TestRobot(CommandBasedRobot):
     def robotInit(self):
         self.xbox_controller = XboxController(constants.xbox_controller_port)
+
         self.drive_train = DriveTrain(self)
+        self.aligner = Aligner(self)
 
     def autonomousInit(self):
         # self.autonomousProgram.start()
